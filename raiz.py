82962bc814b9e061cd1799d57d28a916df60662f
iFallenHunt <lukas_santos.silva@hotmail.com>
2022-07-04 08:50:06 -0300

    Corrected code

--- raiz.py
+++ raiz.py
@@ -1,13 +1,8 @@
 from math import sqrt
 
-
 a = float(input('Enter the value of a: '))
 b = float(input('Enter the value of b: '))
 c = float(input('Enter the value of c: '))
-x1 = float
-x2 = float
-delta = float
-
 
 delta = pow(b, 2) - 4 * a * c
 
@@ -17,8 +12,8 @@ elif delta < 0:
     print('It is not a second degree equation')
 else:
     print('There are two real roots')
-    x1 == (-b + sqrt(delta)) / (2 * a)
-    x2 == (-b - sqrt(delta)) / (2 * a)
+    x1 = (-b + sqrt(delta)) / (2 * a)
+    x2 = (-b - sqrt(delta)) / (2 * a)
     print(f'the root is: ', x1)
     print(f'The root x2 is: ', x2)
     print('The Delta is: ', delta)
